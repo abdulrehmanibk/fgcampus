822968da09e2e0e31a9df48ae5f0b04664054e6f
abdulrehmanibk <abdulrehmanyahya3007@gmail.com>
2025-01-17 21:49:38 +0500

    last-but-not least1

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -5,6 +5,7 @@ import Memorize from './Memorize';
 import Banner from '../../Components/Banner';
 import ShortBoxList from '../../Components/ShortBoxList/ShortBoxList';
 import ContactBanner from '../../Components/ContactBanner/ContactBanner';
+import { Link } from 'react-router-dom';
 
 
 export const Home = () => {
@@ -75,8 +76,7 @@ export const Home = () => {
             para={'In addition to these core schools, students in a given country may also attend schools before and after primary elementary in the and secondary middle school in the us education. Kindergarten or preschool.'}
             children={
               <div style={{display: 'flex'}}>
-                <button className='button2'>Join Now</button>
-                <button className='button1'>View Courses</button>
+                <Link className='button2' to={'/register'}>Join Now</Link>
               </div>
             }
       />
